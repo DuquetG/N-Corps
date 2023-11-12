2059ae5e26638b06c57450fea55fa8510a14933c
DuquetG <GADUQ5@ulaval.ca>
2023-11-12 22:06:44 +0100

    Ajustement de temps

--- Animation2D.py
+++ Animation2D.py
@@ -23,10 +23,10 @@ fig.update_layout(title_text="Positions des masses au fil du temps",
                   showlegend=True,
                   plot_bgcolor='black',
                   paper_bgcolor='black',
-                  template="plotly_dark")
-#                   xaxis=dict(range=[min(data.iloc[:, ::2].values.flatten()), max(data.iloc[:, ::2].values.flatten())]),
-#                   yaxis=dict(range=[min(data.iloc[:, 1::2].values.flatten()), max(data.iloc[:, 1::2].values.flatten())])
-# )
+                  template="plotly_dark",
+                  xaxis=dict(range=[min(data.iloc[:, ::2].values.flatten()), max(data.iloc[:, ::2].values.flatten())]),
+                  yaxis=dict(range=[min(data.iloc[:, 1::2].values.flatten()), max(data.iloc[:, 1::2].values.flatten())])
+)
 
 # Ajouter les frames pour l'animation
 frames = [go.Frame(data=[go.Scatter(x=data.iloc[:frame + 1, i], y=data.iloc[:frame + 1, i + 1],
